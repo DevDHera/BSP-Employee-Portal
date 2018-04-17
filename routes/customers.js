b37acad9ee87f142f09ca429411c12eabcc3afc8
DevDHera <devdhera@gmail.com>
2018-04-18 01:30:46 +0530

    Mail Fix-2

--- routes/customers.js
+++ routes/customers.js
@@ -92,7 +92,7 @@ router.post('/add', ensureAuthenticated, (req, res) => {
                                     // create reusable transporter object using the default SMTP transport
                                     let transporter = nodemailer.createTransport({
                                         host: keys.smtpHost,
-                                        port: keys.smtpPort,
+                                        port: 587,
                                         secure: false, // true for 465, false for other ports
                                         auth: {
                                             user: keys.smtpUser, // generated ethereal user
